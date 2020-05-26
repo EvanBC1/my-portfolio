3b82169219c2630fe96c0c78a0e820b9e092eb6f
Evan Brecht-Curry <starfoxz13@gmail.com>
2020-05-26 15:31:22 -0700

    updated petster and image formatting

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,7 @@ export default function About () {
     <>
       <div id='about' className='about'>
         <h2>About <br/> ━━</h2>
-        <img className='me' src={me} alt="picture of me" />
+        <img className='me' src={me} alt="Evan" />
         <p>I am a Software Engineer with a background in project management and analytics based out of Seattle, Washington. I am passionate about using bleeding-edge technology to build tools that contribute to the long term success of humanity on earth and in space.</p>
       </div>
     </>
